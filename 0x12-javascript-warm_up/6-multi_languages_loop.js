408075457d91bd5bf758f0752317ae48c127e202
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-14 13:29:20 +0100

    Create 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// ['.C IS FUN', '.PY IS COOL', '.JS IS AMAZING'].forEach((e) => console.log(e));
+
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+msg.forEach((element) => console.log(element));
