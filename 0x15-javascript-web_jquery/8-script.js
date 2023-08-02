6db4bc835881fae9f795d851c15a74ed58838d5c
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-08-02 16:16:37 +0100

    Create 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+// it queries API and fetches all the movie titles and also inserts them
+// into the movies list tag
+
+let url = 'https://swapi.co/api/films/?format=json';
+$.get(url, function (data) {
+  let films = data.results;
+  for (let film of films) {
+    $('ul#list_movies').append(`<li>${film.title}</li>`);
+  }
+});
